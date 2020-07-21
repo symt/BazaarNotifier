5008e1afa30b1c74d0776ad1d2fc783783966cf4
Symt <xxrequired@gmail.com>
2020-07-20 21:06:22 -0500

    1.0.1-versions: Updated versions for release

--- src/main/java/dev/meyi/BazaarNotifier.java
+++ src/main/java/dev/meyi/BazaarNotifier.java
@@ -22,7 +22,7 @@ import org.json.JSONTokener;
 public class BazaarNotifier {
 
   public static final String MODID = "BazaarNotifier";
-  public static final String VERSION = "1.0.0";
+  public static final String VERSION = "1.0.1";
   public static String apiKey = "";
 
   public static boolean activeBazaar = false;
