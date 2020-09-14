007d93eaa04fc623ab15609f8ba59363b26425b5
symt <28537235+symt@users.noreply.github.com>
2020-09-14 11:17:32 -0500

    1.3.1: Bug Fixes (#15)

--- src/main/java/dev/meyi/bn/BazaarNotifier.java
+++ src/main/java/dev/meyi/bn/BazaarNotifier.java
@@ -28,7 +28,7 @@ import org.json.JSONTokener;
 public class BazaarNotifier {
 
   public static final String MODID = "BazaarNotifier";
-  public static final String VERSION = "1.3.0";
+  public static final String VERSION = "1.3.1";
   public static final String prefix =
       EnumChatFormatting.GOLD + "[BazaarNotifier] " + EnumChatFormatting.RESET;
   public static String apiKey = "";
@@ -73,7 +73,7 @@ public class BazaarNotifier {
       e.printStackTrace();
     }
 
-    if (Utils.isValidJSONObject(config) && config != null) {
+    if (config != null && Utils.isValidJSONObject(config)) {
       modules = new ModuleList(
           new JSONObject(config));
     } else {
