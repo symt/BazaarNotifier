5fa6c543ab2dedaa1efddd91a1675cd4cd2034ff
Symt <xxrequired@gmail.com>
2020-11-06 12:59:09 -0600

    Perm Fix to Break

--- src/main/java/dev/meyi/bn/utilities/Suggester.java
+++ src/main/java/dev/meyi/bn/utilities/Suggester.java
@@ -8,56 +8,64 @@ import org.json.JSONObject;
 public class Suggester {
 
   public static void basic() {
-    if (BazaarNotifier.validApiKey) {
-      JSONObject bazaarData = BazaarNotifier.bazaarDataRaw;
-      Iterator<String> bazaarKeys = bazaarData.keys();
-      JSONArray bazaarDataFormatted = new JSONArray();
+    try {
+      if (BazaarNotifier.validApiKey) {
+        JSONObject bazaarData = BazaarNotifier.bazaarDataRaw;
+        Iterator<String> bazaarKeys = bazaarData.keys();
+        JSONArray bazaarDataFormatted = new JSONArray();
 
-      while (bazaarKeys.hasNext()) {
-        String key = bazaarKeys.next();
-        JSONObject product = bazaarData.getJSONObject(key);
+        while (bazaarKeys.hasNext()) {
+          String key = bazaarKeys.next();
+          JSONObject product = bazaarData.getJSONObject(key);
 
-        JSONObject currentProduct = new JSONObject()
-            .put("productId", BazaarNotifier.bazaarConversions.getString(key))
-            .put("sellOfferPrice",
-                (bazaarData.getJSONObject(key).getJSONArray("buy_summary").length() > 0
-                    && bazaarData.getJSONObject(key).getJSONArray("sell_summary").length() > 0) ?
-                    bazaarData.getJSONObject(key).getJSONArray("buy_summary").getJSONObject(0)
-                        .getDouble("pricePerUnit") : 0)
-            .put("buyOrderPrice",
-                (bazaarData.getJSONObject(key).getJSONArray("sell_summary").length() > 0
-                    && bazaarData.getJSONObject(key).getJSONArray("buy_summary").length() > 0) ?
-                    bazaarData.getJSONObject(key).getJSONArray("sell_summary").getJSONObject(0)
-                        .getDouble("pricePerUnit") : 0)
-            .put("sellCount",
-                bazaarData.getJSONObject(key).getJSONObject("quick_status")
-                    .getLong("buyMovingWeek"))
-            .put("buyCount",
-                bazaarData.getJSONObject(key).getJSONObject("quick_status")
-                    .getLong("sellMovingWeek"));
+          if (!BazaarNotifier.bazaarConversions.has(key)) {
+            BazaarNotifier.bazaarConversions.put(key, key);
+            BazaarNotifier.bazaarConversionsReversed.put(key, key);
+          }
+          JSONObject currentProduct = new JSONObject()
+              .put("productId", BazaarNotifier.bazaarConversions.getString(key))
+              .put("sellOfferPrice",
+                  (product.getJSONArray("buy_summary").length() > 0
+                      && product.getJSONArray("sell_summary").length() > 0) ?
+                      product.getJSONArray("buy_summary").getJSONObject(0)
+                          .getDouble("pricePerUnit") : 0)
+              .put("buyOrderPrice",
+                  (product.getJSONArray("sell_summary").length() > 0
+                      && product.getJSONArray("buy_summary").length() > 0) ?
+                      product.getJSONArray("sell_summary").getJSONObject(0)
+                          .getDouble("pricePerUnit") : 0)
+              .put("sellCount",
+                  product.getJSONObject("quick_status")
+                      .getLong("buyMovingWeek"))
+              .put("buyCount",
+                  product.getJSONObject("quick_status")
+                      .getLong("sellMovingWeek"));
 
-        double diff = currentProduct.getDouble("sellOfferPrice") * .99d - currentProduct
-            .getDouble("buyOrderPrice");
-        double profitFlowPerMinute =
-            (currentProduct.getLong("sellCount") + currentProduct.getLong("buyCount") == 0) ? 0 :
-                ((currentProduct.getLong("sellCount") * currentProduct.getLong("buyCount")) / (
-                    10080d
-                        * (currentProduct.getLong("sellCount") + currentProduct
-                        .getLong("buyCount"))))
-                    * diff;
-        bazaarDataFormatted.put(currentProduct.put("profitFlowPerMinute", profitFlowPerMinute));
+          double diff = currentProduct.getDouble("sellOfferPrice") * .99d - currentProduct
+              .getDouble("buyOrderPrice");
+          double profitFlowPerMinute =
+              (currentProduct.getLong("sellCount") + currentProduct.getLong("buyCount") == 0) ? 0 :
+                  ((currentProduct.getLong("sellCount") * currentProduct.getLong("buyCount")) / (
+                      10080d
+                          * (currentProduct.getLong("sellCount") + currentProduct
+                          .getLong("buyCount"))))
+                      * diff;
+          bazaarDataFormatted.put(currentProduct.put("profitFlowPerMinute", profitFlowPerMinute));
+        }
+
+        bazaarDataFormatted = Utils.sortJSONArray(bazaarDataFormatted, "profitFlowPerMinute");
+
+        JSONObject bazaarCache = new JSONObject();
+        bazaarDataFormatted.forEach((data) -> {
+          JSONObject jsonData = (JSONObject) data;
+          bazaarCache.put(jsonData.getString("productId").toLowerCase(), data);
+        });
+
+        BazaarNotifier.bazaarCache = bazaarCache;
+        BazaarNotifier.bazaarDataFormatted = bazaarDataFormatted;
       }
-
-      bazaarDataFormatted = Utils.sortJSONArray(bazaarDataFormatted, "profitFlowPerMinute");
-
-      JSONObject bazaarCache = new JSONObject();
-      bazaarDataFormatted.forEach((data) -> {
-        JSONObject jsonData = (JSONObject) data;
-        bazaarCache.put(jsonData.getString("productId").toLowerCase(), data);
-      });
-
-      BazaarNotifier.bazaarCache = bazaarCache;
-      BazaarNotifier.bazaarDataFormatted = bazaarDataFormatted;
+    } catch (Exception e) {
+      e.printStackTrace();
     }
   }
 }
