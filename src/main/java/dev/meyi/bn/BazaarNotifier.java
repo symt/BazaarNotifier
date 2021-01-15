0c96f308aaea5139d1dd6230f51845ee69d752b0
Symt <xxrequired@gmail.com>
2021-01-15 11:18:06 -0600

    New Items Fix

--- src/main/java/dev/meyi/bn/BazaarNotifier.java
+++ src/main/java/dev/meyi/bn/BazaarNotifier.java
@@ -28,7 +28,7 @@ import org.json.JSONTokener;
 public class BazaarNotifier {
 
   public static final String MODID = "BazaarNotifier";
-  public static final String VERSION = "1.3.6";
+  public static final String VERSION = "1.3.7";
   public static final String prefix =
       EnumChatFormatting.GOLD + "[" + EnumChatFormatting.YELLOW + "BN" + EnumChatFormatting.GOLD + "] " + EnumChatFormatting.RESET;
   public static String apiKey = "";
