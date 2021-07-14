7dddb5e46a9bae8caf819ad7ee5b8ad9ab105972
symt <28537235+symt@users.noreply.github.com>
2021-07-14 16:58:02 -0500

    1.4.3: Crystal Hollows Items (#43)

--- src/main/java/dev/meyi/bn/BazaarNotifier.java
+++ src/main/java/dev/meyi/bn/BazaarNotifier.java
@@ -28,7 +28,7 @@ import org.json.JSONTokener;
 public class BazaarNotifier {
 
   public static final String MODID = "BazaarNotifier";
-  public static final String VERSION = "1.4.2";
+  public static final String VERSION = "1.4.3";
   public static final String prefix =
       EnumChatFormatting.GOLD + "[" + EnumChatFormatting.YELLOW + "BN" + EnumChatFormatting.GOLD + "] " + EnumChatFormatting.RESET;
   public static String apiKey = "";
