21333106818186be0bcdba896e2bc2918a9644d5
Detlev1 <julian_be@gmx.de>
2022-01-23 21:26:23 +0100

    Fix

--- src/main/java/dev/meyi/bn/modules/calc/BankCalculator.java
+++ src/main/java/dev/meyi/bn/modules/calc/BankCalculator.java
@@ -1,66 +1,143 @@
-package dev.meyi.bn.modules;
+package dev.meyi.bn.modules.calc;
 
-import com.google.gson.JsonObject;
+
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import dev.meyi.bn.BazaarNotifier;
-import dev.meyi.bn.config.ModuleConfig;
-import dev.meyi.bn.utilities.ColorUtils;
-import dev.meyi.bn.utilities.Defaults;
-import dev.meyi.bn.modules.calc.BankCalculator;
-import java.awt.Color;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 import net.minecraft.client.Minecraft;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.scoreboard.Score;
+import net.minecraft.scoreboard.ScoreObjective;
+import net.minecraft.scoreboard.ScorePlayerTeam;
+import net.minecraft.scoreboard.Scoreboard;
+import net.minecraft.util.StringUtils;
 
-public class BankModule extends Module {
-  public static final ModuleName type = ModuleName.BANK;
 
-  public BankModule() {
-    super();
+public class BankCalculator {
+
+  public static double bank = 0;
+  private static double moneyOnStartup =
+          moneyStoredInSellOffers() + moneyStoredInBuyOrders() + getPurse();
+  public static double bazaarProfit = 0;
+
+  public static double calculateProfit() {
+    return getPurse() + moneyStoredInBuyOrders() + moneyStoredInSellOffers() + bank
+            - moneyOnStartup;
   }
 
-  public BankModule(ModuleConfig config) {
-    super(config);
-  }
 
-
-  @Override
-  protected void draw() {
-    List<LinkedHashMap<String, Color>> items = new ArrayList<>();
-
-    LinkedHashMap<String, Color> message = new LinkedHashMap<>();
-    message.put("Total profit: ", Color.CYAN);
-    message.put(BazaarNotifier.df.format((int) BankCalculator.calculateProfit()), Color.MAGENTA);
-    items.add(message);
-    LinkedHashMap<String, Color> message2 = new LinkedHashMap<>();
-    message2.put("Bazaar profit: ", Color.CYAN);
-    message2.put(BazaarNotifier.df.format(BankCalculator.bazaarProfit) , Color.MAGENTA);
-    items.add(message2);
-
-    int longestXString = ColorUtils.drawColorfulParagraph(items, x, y, scale);
-    boundsX = x + longestXString;
-    boundsY = (int) (y + (Minecraft.getMinecraft().fontRendererObj.FONT_HEIGHT * 2) * scale);
-  }
-
-  @Override
-  protected void reset() {
-    x = Defaults.BANK_MODULE_X;
-    y = Defaults.BANK_MODULE_Y;
-    scale = 1;
-  }
-
-  @Override
-  protected String name() {
-    return ModuleName.BANK.name();
-  }
-
-  @Override
-  protected boolean shouldDrawBounds() {
-    return true;
-  }
-
-  @Override
-  protected int getMaxShift() {
+  public static double moneyStoredInSellOffers() {
+    if (BazaarNotifier.orders.size() != 0) {
+      double orderWorth = 0;
+      for (int i = 0; i < BazaarNotifier.orders.size(); i++) {
+        if (BazaarNotifier.orders.get(i).type.equals("sell")) {
+          orderWorth += BazaarNotifier.orders.get(i).orderValue;
+        }
+      }
+      return orderWorth;
+    }
     return 0;
+  }
+
+  public static double moneyStoredInBuyOrders() {
+    if (BazaarNotifier.orders.size() != 0) {
+      double orderWorth = 0;
+      for (int i = 0; i < BazaarNotifier.orders.size(); i++) {
+        if (BazaarNotifier.orders.get(i).type.equals("buy")) {
+          orderWorth += BazaarNotifier.orders.get(i).orderValue;
+        }
+      }
+      return orderWorth;
+    }
+    return 0;
+  }
+
+  public static double getPurse() {
+    if (getPurseFromSidebar() == -1) {
+      return getPurseFromAPI();
+    } else {
+      return getPurseFromSidebar();
+    }
+  }
+
+  public static double getPurseFromAPI() {
+    if (BazaarNotifier.playerDataFromAPI.entrySet().size() != 0) {
+      return BazaarNotifier.playerDataFromAPI.get("coin_purse").getAsDouble();
+    }
+    return -1;
+  }
+
+  private static double getPurseFromSidebar() {
+    Scoreboard scoreboard = Minecraft.getMinecraft().theWorld.getScoreboard();
+    if (scoreboard == null) {
+      return -1;
+    }
+
+    ScoreObjective objective = scoreboard.getObjectiveInDisplaySlot(1);
+
+    if (objective == null) {
+      return -1;
+    }
+
+    Collection<Score> scores = scoreboard.getSortedScores(objective);
+    List<Score> list = scores.stream()
+            .filter(input -> input != null && input.getPlayerName() != null && !input.getPlayerName()
+                    .startsWith("#")).collect(Collectors.toList());
+
+    if (list.size() > 15) {
+      scores = Lists.newArrayList(Iterables.skip(list, scores.size() - 15));
+    } else {
+      scores = list;
+    }
+
+    for (Score score : scores) {
+      ScorePlayerTeam team = scoreboard.getPlayersTeam(score.getPlayerName());
+      if (ScorePlayerTeam.formatPlayerName(team, score.getPlayerName()).contains("Purse")) {
+        if (ScorePlayerTeam.formatPlayerName(team, score.getPlayerName())
+                .contains(")")) {// coins get added to your purse
+          String purse = StringUtils
+                  .stripControlCodes(ScorePlayerTeam.formatPlayerName(team, score.getPlayerName()));
+          int i = purse.indexOf("(");
+          String s = purse.substring(i + 1, purse.length() - 1);
+          purse = purse.replace(s, "").replaceAll("[^0-9 .]", "");
+          return Float.parseFloat(purse);
+        } else {
+          String purse = StringUtils
+                  .stripControlCodes(ScorePlayerTeam.formatPlayerName(team, score.getPlayerName()))
+                  .replaceAll("[^0-9 +.]", "");
+          return Float.parseFloat(purse);
+        }
+      }
+    }
+
+    return -1;
+  }
+
+  public static void extractBankFromItemDescription(IInventory chest) {
+    if (chest != null) {
+      if (chest.getStackInSlot(11) != null) {
+        if (chest.getStackInSlot(11).getDisplayName().toLowerCase().contains("deposit coins")) {
+          if (bank == 0) {
+            bank = Double.parseDouble(StringUtils.stripControlCodes(
+                    chest.getStackInSlot(11).getTagCompound().getCompoundTag("display")
+                            .getTagList("Lore", 8)
+                            .getStringTagAt(0)).split("balance: ")[1].replaceAll(",", ""));
+            moneyOnStartup += bank;
+          }
+          bank = Double.parseDouble(StringUtils.stripControlCodes(
+                  chest.getStackInSlot(11).getTagCompound().getCompoundTag("display")
+                          .getTagList("Lore", 8)
+                          .getStringTagAt(0)).split("balance: ")[1].replaceAll(",", ""));
+        }
+      }
+    }
+  }
+
+  public static void reset() {
+    moneyOnStartup = getPurse() + moneyStoredInBuyOrders() + moneyStoredInSellOffers() + bank;
+    bazaarProfit = 0;
   }
 }
