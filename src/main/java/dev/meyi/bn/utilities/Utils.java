b32d489331877a6e32b5132273e34cfaf3e8ab6a
Symt <xxrequired@gmail.com>
2022-05-05 00:30:39 -0500

    1.4.10: Fixes

--- src/main/java/dev/meyi/bn/utilities/Utils.java
+++ src/main/java/dev/meyi/bn/utilities/Utils.java
@@ -9,14 +9,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import javax.net.ssl.HttpsURLConnection;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
@@ -25,7 +23,6 @@ import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -34,19 +31,18 @@ import org.json.JSONObject;
 public class Utils {
 
   public static JSONObject getBazaarData() throws IOException {
+    HttpClient client = HttpClientBuilder.create().build();
     String apiBit = "";
     if (!BazaarNotifier.apiKeyDisabled) {
       apiBit = "?key=" + BazaarNotifier.apiKey;
     }
-
-    HttpsURLConnection connection = (HttpsURLConnection) new URL("https://api.hypixel.net/skyblock/bazaar" + apiBit).openConnection();
-    connection.setSSLSocketFactory(BazaarNotifier.sslSocketFactory);
-    connection.connect();
+    HttpGet request = new HttpGet(
+        "https://api.hypixel.net/skyblock/bazaar" + apiBit);
+    HttpResponse response = client.execute(request);
 
     String result = IOUtils.toString(new BufferedReader
         (new InputStreamReader(
-            connection.getInputStream())));
-
+            response.getEntity().getContent())));
 
     return new JSONObject(result).getJSONObject("products");
   }
